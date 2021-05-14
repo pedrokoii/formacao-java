50634a5a1c16e768b591e77ccc2b3069ff94d17a
Pedro Freitas <koii.tkd@gmail.com>
2021-05-14 12:13:05 -0300

    Aula Construtores

--- 1. Java e Orientação a Objetos/02. Introdução à Orientação a Objetos/bytebank-encapsulado/src/Conta.java
+++ 1. Java e Orientação a Objetos/02. Introdução à Orientação a Objetos/bytebank-encapsulado/src/Conta.java
@@ -4,12 +4,19 @@ public class Conta {
     private int numero;
     private Cliente titular;
 
+    public Conta(int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("estou criando uma conta " + this.numero);
+
+    }
+
     public void deposita(double valor) {
         this.saldo += valor;
     }
 
     public boolean saca(double valor) {
-        if(this.saldo >= valor){
+        if (this.saldo >= valor) {
             this.saldo -= valor;
             return true;
         } else {
@@ -17,31 +24,39 @@ public class Conta {
         }
     }
 
-    public boolean transfere(double valor, Conta destino){
-        if(this.saca(valor)) {
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saca(valor)) {
             destino.deposita(valor);
             return true;
         }
         return false;
     }
 
-    public double getSaldo(){
+    public double getSaldo() {
         return this.saldo;
     }
 
-    public int getNumero(){
+    public int getNumero() {
         return this.numero;
     }
 
-    public void setNumero(int numero){
+    public void setNumero(int numero) {
+        if (numero <= 0) {
+            System.out.println("nao pode valor menor ou igual a 0");
+            return;
+        }
         this.numero = numero;
     }
-    
+
     public int getAgencia() {
         return agencia;
     }
 
     public void setAgencia(int agencia) {
+        if (agencia <= 0) {
+            System.out.println("nao pode valor menor ou igual a 0");
+            return;
+        }
         this.agencia = agencia;
     }
 

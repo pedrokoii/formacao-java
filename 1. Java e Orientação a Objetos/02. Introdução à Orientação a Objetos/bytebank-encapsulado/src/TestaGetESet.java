bd29e111401ae79b395be7f180bd9568356b39b7
Pedro Freitas <koii.tkd@gmail.com>
2021-05-14 11:47:49 -0300

    Aula Getters e Setters de referência

--- 1. Java e Orientação a Objetos/02. Introdução à Orientação a Objetos/bytebank-encapsulado/src/TestaGetESet.java
+++ 1. Java e Orientação a Objetos/02. Introdução à Orientação a Objetos/bytebank-encapsulado/src/TestaGetESet.java
@@ -4,5 +4,21 @@ public class TestaGetESet {
         //conta.numero = 1337;
         conta.setNumero(1337);
         System.out.println(conta.getNumero());
+
+        Cliente paulo = new Cliente();
+        paulo.setNome("paulo silveira");
+        //conta.titular = paulo;
+        conta.setTitular(paulo);
+        System.out.println(conta.getTitular().getNome());
+
+        //setando profissao em uma linha
+        conta.getTitular().setProfissao("programador");
+        //agora em duas linhas
+        Cliente titularDaConta = conta.getTitular();
+        titularDaConta.setProfissao("programador");
+
+        System.out.println(titularDaConta);
+        System.out.println(paulo);
+        System.out.println(conta.getTitular());
     }
 }

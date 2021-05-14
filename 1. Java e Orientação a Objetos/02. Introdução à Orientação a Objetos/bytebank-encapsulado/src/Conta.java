29dc27c22c6b9014c82d5d144f1047eb2a93ebe6
Pedro Freitas <koii.tkd@gmail.com>
2021-05-14 12:40:28 -0300

    Aula Static

--- 1. Java e Orientação a Objetos/02. Introdução à Orientação a Objetos/bytebank-encapsulado/src/Conta.java
+++ 1. Java e Orientação a Objetos/02. Introdução à Orientação a Objetos/bytebank-encapsulado/src/Conta.java
@@ -3,12 +3,15 @@ public class Conta {
     private int agencia;
     private int numero;
     private Cliente titular;
+    private static int total;
 
     public Conta(int agencia, int numero) {
+        Conta.total++;
+        //System.out.println("o total de contas é " + Conta.total);
+
         this.agencia = agencia;
         this.numero = numero;
-        System.out.println("estou criando uma conta " + this.numero);
-
+        //System.out.println("estou criando uma conta " + this.numero);
     }
 
     public void deposita(double valor) {
@@ -66,5 +69,9 @@ public class Conta {
 
     public Cliente getTitular() {
         return titular;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
     }
 }

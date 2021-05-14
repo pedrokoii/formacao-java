7399f5a3c79bc6bf6a29e059b10b46c0683b267c
Pedro Freitas <koii.tkd@gmail.com>
2021-05-14 15:56:44 -0300

    Aula Começando com a herança

---
+++ 1. Java e Orientação a Objetos/03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
@@ -0,0 +1,20 @@
+public class Gerente extends Funcionario { // Gerente eh um Funcionario, Gerente herda da classe Funcionario
+
+    private int senha;
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // public double getBonificacao() {
+    //     return this.salario;
+    // }
+}

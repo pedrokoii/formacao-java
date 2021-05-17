7d69716226fba5752c61b82f29bc43d9541f32e9
Pedro Freitas <koii.tkd@gmail.com>
2021-05-17 14:17:23 -0300

    Aula Completando o sistema

--- 1. Java e Orientação a Objetos/03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
+++ 1. Java e Orientação a Objetos/03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
@@ -1,8 +1,26 @@
-public class Gerente extends Funcionario { // Gerente eh um FuncionarioAutenticavel, Gerente herda da classe FuncionarioAutenticavel
+// Gerente eh um Funcionario, Gerente herda da classe Funcionario, assina o contrato Autenticavel, eh um Autenticavel
+public class Gerente extends Funcionario implements Autenticavel{ 
+
+    private int senha;
 
     @Override
     public double getBonificacao() {
         System.out.println("chamando o método de bonificacao do GERENTE");
         return super.getSalario();
     }
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+    
+    @Override
+    public boolean autentica(int senha)  {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }

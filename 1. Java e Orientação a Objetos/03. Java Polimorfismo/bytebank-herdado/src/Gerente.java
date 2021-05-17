3bcf68bda429d798d8c2b43e9152f511756838d4
Pedro Freitas <koii.tkd@gmail.com>
2021-05-17 13:56:20 -0300

    Aula Herança múltipla?

--- 1. Java e Orientação a Objetos/03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
+++ 1. Java e Orientação a Objetos/03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
@@ -1,4 +1,4 @@
-public class Gerente extends FuncionarioAutenticavel { // Gerente eh um FuncionarioAutenticavel, Gerente herda da classe FuncionarioAutenticavel
+public class Gerente extends Funcionario { // Gerente eh um FuncionarioAutenticavel, Gerente herda da classe FuncionarioAutenticavel
 
     public double getBonificacao() {
         System.out.println("chamando o método de bonificacao do GERENTE");

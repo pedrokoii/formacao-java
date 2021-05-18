78fc8fcce0f4e3c5975d9f97e7d785e307a3b7c7
Pedro Freitas <koii.tkd@gmail.com>
2021-05-18 11:41:03 -0300

    Aula Introdução a exceções

--- 1. Java e Orientação a Objetos/06. Exceções/java-pilha/src/Fluxo.java
+++ 1. Java e Orientação a Objetos/06. Exceções/java-pilha/src/Fluxo.java
@@ -1,24 +1,22 @@
 public class Fluxo {
 
     public static void main(String[] args) {
-        //adicionar breakpoint na linha abaixo
-        System.out.println("Ini do main");
+        System.out.println("Ini do main"); //adicionar breakpoint
         metodo1();
         System.out.println("Fim do main");
     }
     
     private static void metodo1() {
-        //adicionar breakpoint na linha abaixo
-        System.out.println("Ini do metodo1");
+        System.out.println("Ini do metodo1"); //adicionar breakpoint
         metodo2();
         System.out.println("Fim do metodo1");
     }
     
     private static void metodo2() {
-        //adicionar breakpoint na linha abaixo
-        System.out.println("Ini do metodo2");
+        System.out.println("Ini do metodo2"); //adicionar breakpoint
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            int a = i / 0; //insere exceção aritmética
         }
         System.out.println("Fim do metodo2");
     }

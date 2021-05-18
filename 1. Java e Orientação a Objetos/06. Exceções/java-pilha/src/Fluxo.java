ea7fb95a0e10271091dc2cb77470db0821c8e919
Pedro Freitas <koii.tkd@gmail.com>
2021-05-18 11:19:02 -0300

    Aula Depuração

--- 1. Java e Orientação a Objetos/06. Exceções/java-pilha/src/Fluxo.java
+++ 1. Java e Orientação a Objetos/06. Exceções/java-pilha/src/Fluxo.java
@@ -1,18 +1,21 @@
 public class Fluxo {
 
     public static void main(String[] args) {
+        //adicionar breakpoint na linha abaixo
         System.out.println("Ini do main");
         metodo1();
         System.out.println("Fim do main");
     }
-
+    
     private static void metodo1() {
+        //adicionar breakpoint na linha abaixo
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
-
+    
     private static void metodo2() {
+        //adicionar breakpoint na linha abaixo
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);

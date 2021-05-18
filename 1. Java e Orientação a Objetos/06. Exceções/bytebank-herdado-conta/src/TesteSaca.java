b0410c897c0f5d420b8c599e9426905868270896
Pedro Freitas <koii.tkd@gmail.com>
2021-05-18 15:45:17 -0300

    Aula Sacando com Checked Exception

--- 1. Java e Orientação a Objetos/06. Exceções/bytebank-herdado-conta/src/TesteSaca.java
+++ 1. Java e Orientação a Objetos/06. Exceções/bytebank-herdado-conta/src/TesteSaca.java
@@ -4,7 +4,11 @@ public class TesteSaca {
         Conta conta = new ContaCorrente(123, 321);
 
         conta.deposita(200.0);
-        conta.saca(200.0);
+        try {
+            conta.saca(210.0);
+        } catch (SaldoInsuficienteException e) {
+            System.out.println("Ex: " + e.getMessage());
+        }
 
         System.out.println(conta.getSaldo());
 

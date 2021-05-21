91f8147fcd9a92790892c3850cd8a7e3e5dac329
Pedro Freitas <koii.tkd@gmail.com>
2021-05-21 15:18:32 -0300

    Aula Metodos da classe String

--- 1. Java e Orientacao a Objetos/07. Classes Object e String/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
+++ 1. Java e Orientacao a Objetos/07. Classes Object e String/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -4,15 +4,40 @@ public class TesteString {
     
     public static void main(String[] args) {
         
-        String nome = "Alura"; //object literal
+        String aluraEspacos = "     Alura     ";
+        String aluraSemEspacos = aluraEspacos.trim();
+
+        System.out.println(aluraEspacos);
+        System.out.println(aluraSemEspacos);
+
+        String vazio = "";
+        String espaco = " ";
+
+        System.out.println(vazio.isEmpty());
+        System.out.println(espaco.isEmpty());
+
         //String outro = new String("Alura");
-
-        String replace = nome.replace("A", "a");
-        String lower = nome.toLowerCase();
-
+        String nome = "Alura"; //object literal
         System.out.println(nome);
-        System.out.println(replace);
-        System.out.println(lower);
+
+        System.out.println(nome.length());
+        for(int i=0; i < nome.length(); i++){
+            System.out.println(nome.charAt(i));
+        }
+
+        System.out.println(nome.substring(1));
+
+        System.out.println(nome.indexOf("ur"));
+
+        char c = nome.charAt(2);
+        System.out.println(c);
+
+        char x = 'A';
+        char y = 'a';
+        System.out.println(nome.replace(x, y));
+
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
 
     }
 

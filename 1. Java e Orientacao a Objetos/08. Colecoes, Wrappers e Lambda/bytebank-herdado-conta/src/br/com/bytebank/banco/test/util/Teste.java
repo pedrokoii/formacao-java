eb6c7d294a211771701f3abe13b462ed0e9280a9
Pedro Freitas <koii.tkd@gmail.com>
2021-05-24 16:53:19 -0300

    Aula Métodos da classe Integer

--- 1. Java e Orientacao a Objetos/08. Colecoes, Wrappers e Lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ 1. Java e Orientacao a Objetos/08. Colecoes, Wrappers e Lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -6,12 +6,22 @@ import java.util.List;
 public class Teste {
     
     public static void main(String[] args) {
-        
-        int[] idades = new int[5];
-        String[] nomes = new String[5];
 
-        int idade = 29; //Integer
-        Integer idadeRef = new Integer(29);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.BYTES);
+
+        int idade = 29;
+        Integer idadeRef = Integer.valueOf(idade); //Autoboxing
+        System.out.println(idadeRef.doubleValue());
+        int valor = idadeRef.intValue(); //Unboxing
+        System.out.println(valor);
+
+        String s = "10";
+        //Integer numero = Integer.valueOf(s);
+        int numero = Integer.parseInt(s);
+        System.out.println(numero);
 
         List<Integer> numeros = new ArrayList<Integer>();
         numeros.add(29); //Autoboxing

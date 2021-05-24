659bd102536d9092b05e407eed9d17c40442e460
Pedro Freitas <koii.tkd@gmail.com>
2021-05-24 14:25:17 -0300

    Aula Introdução ao Generics

--- 1. Java e Orientacao a Objetos/08. Colecoes, Wrappers e Lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ 1. Java e Orientacao a Objetos/08. Colecoes, Wrappers e Lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -9,7 +9,8 @@ public class Teste {
     
     public static void main(String[] args) {
         
-        ArrayList lista = new ArrayList();
+        //Generics
+        ArrayList<Conta> lista = new ArrayList<Conta>();
 
         Conta cc = new ContaCorrente(22, 11);
         lista.add(cc);
@@ -18,8 +19,7 @@ public class Teste {
         lista.add(cc2);
 
         System.out.println("Tamanho: " + lista.size());
-
-        Conta ref = (Conta) lista.get(0);
+        Conta ref = lista.get(0);
         System.out.println(ref.getNumero());
 
         lista.remove(0);
@@ -38,8 +38,8 @@ public class Teste {
 
         System.out.println("-------------");
 
-        for(Object oRef : lista) {
-            System.out.println(oRef);
+        for(Conta conta : lista) {
+            System.out.println(conta);
         }
     }
 

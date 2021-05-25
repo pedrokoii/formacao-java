75e72b575f8215cbb390a97e0c359ff90e341e67
Pedro Freitas <koii.tkd@gmail.com>
2021-05-25 12:52:04 -0300

    Aula Finalmente Lambdas

--- 1. Java e Orientacao a Objetos/08. Colecoes, Wrappers e Lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteClasseAnonima.java
+++ 1. Java e Orientacao a Objetos/08. Colecoes, Wrappers e Lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteClasseAnonima.java
@@ -9,7 +9,7 @@ import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
-public class Teste {
+public class TesteClasseAnonima {
 
     public static void main(String[] args) {
 

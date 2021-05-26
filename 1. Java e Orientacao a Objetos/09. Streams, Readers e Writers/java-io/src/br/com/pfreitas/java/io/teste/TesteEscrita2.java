2ebc8f97b01c2ae550311913b328f090ab1217e9
Pedro Freitas <koii.tkd@gmail.com>
2021-05-26 10:27:02 -0300

    Aula Saída com FileWriter

---
+++ 1. Java e Orientacao a Objetos/09. Streams, Readers e Writers/java-io/src/br/com/pfreitas/java/io/teste/TesteEscrita2.java
@@ -0,0 +1,28 @@
+package br.com.pfreitas.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TesteEscrita2 {
+
+    public static void main(String[] args) throws IOException {
+        
+        //Fluxo de Saida com Arquivo
+        // OutputStream fos = new FileOutputStream("lorem2.txt");
+        // Writer osw = new OutputStreamWriter(fos);
+        // BufferedWriter bw = new BufferedWriter(osw);
+
+        FileWriter fw = new FileWriter("lorem2.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        bw.newLine();
+        bw.newLine();
+        bw.write("Teste bacanildo");
+
+        bw.close();
+
+    }
+    
+}
